160c9056bdb6d78b75b17436e5702177c0055fc7
svaronc <sebastianvaron96@gmail.com>
2024-02-22 19:26:03 -0500

    initial commit

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,6 @@
 class WelcomeController < ApplicationController
   def hello
+    @creadentials = Rails.application.credentials.hello
+    @variable = "hello i am " +  Pet.first.name + " and i am a " + Pet.first.breed
   end
 end
